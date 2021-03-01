fcb081b8ab3555371decc30bb14c6e83682e4248
David <dop4398@rit.edu>
2021-03-01 06:40:49 -0500

    camera works. player moves and jumps poorly.

---
+++ 3D Platformer Prototype/Assets/Scripts/CameraController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // taken and modified from here: https://faramira.com/a-configurable-third-person-camera-in-unity/
+
+    #region Field Declarations
+    public Transform mPlayer;
+
+    public Vector3 mPositionOffset = new Vector3(0.0f, 1.0f, -3.0f);
+    public Vector3 mAngleOffset = new Vector3(0.0f, 0.0f, 0.0f);
+    [Tooltip("The damping factor to smooth the changes in position and rotation of the camera.")]
+    public float mDamping = 10.0f;
+
+    public float mMinPitch = -30.0f;
+    public float mMaxPitch = 30.0f;
+    public float mRotationSpeed = 2.0f;
+    private float angleX = 0.0f;
+    #endregion
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    void LateUpdate()
+    {
+        Follow_IndependentRotation();
+    }
+
+
+    void Follow_IndependentRotation()
+    {
+        float mx, my;
+        mx = Input.GetAxis("Mouse X");
+        my = Input.GetAxis("Mouse Y");
+
+        // We apply the initial rotation to the camera.
+        Quaternion initialRotation = Quaternion.Euler(mAngleOffset);
+
+        Vector3 eu = transform.rotation.eulerAngles;
+
+        angleX -= my * mRotationSpeed;
+
+        // We clamp the angle along the X axis to be between the min and max pitch.
+        angleX = Mathf.Clamp(angleX, mMinPitch, mMaxPitch);
+
+        eu.y += mx * mRotationSpeed;
+        Quaternion newRot = Quaternion.Euler(angleX, eu.y, 0.0f) * initialRotation;
+
+        transform.rotation = newRot;
+
+        Vector3 forward = transform.rotation * Vector3.forward;
+        Vector3 right = transform.rotation * Vector3.right;
+        Vector3 up = transform.rotation * Vector3.up;
+
+        Vector3 targetPos = mPlayer.position;
+        Vector3 desiredPosition = targetPos
+            + forward * mPositionOffset.z
+            + right * mPositionOffset.x
+            + up * mPositionOffset.y;
+
+        Vector3 position = Vector3.Lerp(transform.position,
+            desiredPosition,
+            Time.deltaTime * mDamping);
+        transform.position = position;
+    }
+}
--- 3D Platformer Prototype/Assets/Scripts/PlayerController.cs
+++ 3D Platformer Prototype/Assets/Scripts/PlayerController.cs
@@ -7,24 +7,25 @@ public class PlayerController : MonoBehaviour
     // put events here if necessary
 
     #region Field Declarations
-    public Camera camera;
-    [SerializeField] private float mass;
-    [SerializeField] private float maxSpeed;
-    [SerializeField] private float direction;
-    [SerializeField] private float acceleration;
+    [SerializeField] private Camera camera;
+    [SerializeField] private Rigidbody body;
+    [SerializeField] private float maxSpeed = 1.0f;
+    [SerializeField] private float jumpForce = 100.0f;
+    [SerializeField] private Vector3 gravity = new Vector3(0.0f, -0.1f, 0.0f);
 
-
+    private bool grounded = false;
+    private Vector3 cameraForward;
     #endregion
 
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
     }
 
     
     void Update()
     {
-        
+        MovePlayer();
     }
 
     /* TODO:
@@ -37,4 +38,88 @@ public class PlayerController : MonoBehaviour
      * When crouched and moving in a direction and the player jumps, do unique jumps.
      * If airborne and a collision with a wall is detected, bonk off the wall - set velocity to 0, add a force opposite to the previous velocity, deccel does the rest.
     */
+
+    
+    private void MovePlayer()
+    {
+        // Only apply drag if on the ground. ignore if airborne.
+        // eventually will have some conditionals for applying lateral forces
+        MoveLateral();
+
+        // jump goes here
+        Jump();
+    }
+
+    private void MoveLateral()
+    {
+        cameraForward = new Vector3(camera.transform.forward.x, 0.0f, camera.transform.forward.z);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            body.AddForce(cameraForward * maxSpeed);
+        } 
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            body.AddForce(cameraForward * -1 * maxSpeed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            body.AddForce(camera.transform.right * -1 * maxSpeed);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            body.AddForce(camera.transform.right * maxSpeed);
+        }
+    }
+
+    private void ApplyDrag()
+    {
+        body.velocity -= body.velocity * 0.01f;
+    }
+
+    private void AddVelocity(Vector3 v)
+    {
+        body.velocity += v;
+    }
+
+    private void Jump()
+    {
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            grounded = false;
+            body.AddForce(transform.up * jumpForce);
+        }
+        else if(!grounded) // apply gravity
+        {
+            body.velocity += gravity;
+        }
+    }
+
+    private void WallBonk()
+    {
+        body.velocity = Vector3.zero;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+
+        // check for bonk
+        if (!grounded && collision.gameObject.CompareTag("Wall"))
+        {
+            WallBonk();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = false;
+        }
+    }
 }
