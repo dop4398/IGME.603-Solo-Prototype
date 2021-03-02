457178084558f165ca3ef016b22f272b4d469d17
David <dop4398@rit.edu>
2021-03-01 22:00:52 -0500

    we have something.
    added enemies. multiple types of jumps. bonking off walls and enemies. good stuff.

--- 3D Platformer Prototype/Assets/Scripts/EnemyController.cs
+++ 3D Platformer Prototype/Assets/Scripts/EnemyController.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject parent;
+
+    private float moveTimer = 0.0f;
+    private Vector3 direction;
+    [SerializeField] private float moveForce = 5.0f;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        moveTimer += Time.deltaTime;
+
+        if(moveTimer > 2.0f)
+        {
+            direction = Random.onUnitSphere;
+            direction.y = 0.0f;
+            parent.GetComponent<Rigidbody>().AddForce(direction * moveForce, ForceMode.Impulse);
+            moveTimer = 0.0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(parent);
+        }
     }
 }
--- 3D Platformer Prototype/Assets/Scripts/PlayerController.cs
+++ 3D Platformer Prototype/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,26 +11,41 @@ public class PlayerController : MonoBehaviour
     #region Field Declarations
     [SerializeField] private Camera camera;
     [SerializeField] private Rigidbody body;
+    private Vector3 cameraForward;
+    private Vector3 cameraRight;
+
     [SerializeField] private float acceleration = 0.01f;
     [SerializeField] private float deceleration = 0.01f;
-    [SerializeField] private float maxSpeed = 1.0f;
+    private float maxSpeed = 1.0f;
+    [SerializeField] private float runSpeed = 10.0f;
+    [SerializeField] private float crouchSpeed = 5.0f;
+
     [SerializeField] private float jumpForce = 100.0f;
     [SerializeField] private Vector3 gravity = new Vector3(0.0f, -0.1f, 0.0f);
 
-    private bool grounded = false;
-    private Vector3 cameraForward;
-    private Vector3 cameraRight;
+    [SerializeField] private Vector3 velocity = Vector3.zero;
+    [SerializeField] private Vector2 velocity2D = Vector2.zero;
+
+    [SerializeField] private bool grounded = false;
+    [SerializeField] private bool moving = false;
+    [SerializeField] private float jumpTimer = 0.0f;
+    [SerializeField] private float jumpTimeLimit = 0.5f;
+    [SerializeField] private int jumpStage = 0;
     #endregion
 
     void Start()
     {
         body = GetComponent<Rigidbody>();
+        velocity2D = new Vector2(body.velocity.x, body.velocity.z);
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     
     void Update()
     {
         MovePlayer();
+
+        LockMouse();
     }
 
     /* TODO:
@@ -52,6 +68,12 @@ public class PlayerController : MonoBehaviour
 
         // jump goes here
         Jump();
+
+        // reset if stuck
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            transform.position = new Vector3(12.0f, 2.0f, 8.0f);
+        }
     }
 
     private void MoveLateral()
@@ -59,29 +81,150 @@ public class PlayerController : MonoBehaviour
         cameraForward = new Vector3(camera.transform.forward.x, 0.0f, camera.transform.forward.z);
         cameraRight = new Vector3(camera.transform.right.x, 0.0f, camera.transform.right.z);
 
+        if(Input.GetKey(KeyCode.LeftControl))
+        {
+            maxSpeed = crouchSpeed;
+        }
+        else
+        {
+            maxSpeed = runSpeed;
+        }
+
         // while a key is down, accelerate to maxSpeed
         if (Input.GetKey(KeyCode.W))
         {
             Accelerate(cameraForward);
+            moving = true;
         }
         else if (Input.GetKey(KeyCode.S))
         {
             Accelerate(cameraForward * -1.0f);
+            moving = true;
         }
+        
 
         if (Input.GetKey(KeyCode.A))
         {
             Accelerate(cameraRight * -1.0f);
+            moving = true;
         }
         else if (Input.GetKey(KeyCode.D))
         {
             Accelerate(cameraRight);
+            moving = true;
         }
 
         // otherwise, decelerate when grounded and a key isn't pressed
         if (grounded && !(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
         {
             Decelerate();
+            moving = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+
+        }
+    }
+
+
+    private void Jump()
+    {
+        if (grounded)
+        {
+            jumpTimer += Time.deltaTime;
+        }
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Input.GetKey(KeyCode.LeftControl)) // crouch jumps
+            {
+                if (moving)
+                {
+                    body.AddForce(transform.up * jumpForce * 0.5f);
+                    AddVelocity(body.velocity.normalized * jumpForce);
+                }
+                else
+                {
+                    body.AddForce(transform.up * jumpForce * 2.0f);
+                    body.AddForce(camera.transform.forward * jumpForce * -0.25f);
+                }
+            }
+            else
+            {
+                switch (jumpStage)
+                {
+                    case 1:
+                        if (jumpTimer < jumpTimeLimit)
+                        {
+                            jumpStage = 2;
+                            body.AddForce(transform.up * jumpForce * 1.5f);
+                        }
+                        else
+                        {
+                            body.AddForce(transform.up * jumpForce);
+                        }
+                        break;
+                    case 2:
+                        if (jumpTimer < jumpTimeLimit)
+                        {
+                            body.AddForce(transform.up * jumpForce * 2.0f);
+                        }
+                        else
+                        {
+                            body.AddForce(transform.up * jumpForce);
+                        }
+                        jumpStage = 0;
+                        break;
+                    default:
+                        body.AddForce(transform.up * jumpForce);
+                        jumpStage = 1;
+                        break;
+                }
+                jumpTimer = 0.0f;
+            }
+            grounded = false;
+        }
+        else if (!grounded) // apply gravity
+        {
+            AddVelocity(gravity);
+        }
+        else
+        {
+            body.velocity = new Vector3(body.velocity.x, 0.0f, body.velocity.z);
+        }
+    }
+
+    private void WallBonk(Vector3 v)
+    {
+        AddVelocity(new Vector3(v.x, 0.0f, v.z).normalized * maxSpeed * 50.0f);
+    }
+
+    private void EnemyStomp()
+    {
+        body.AddForce(transform.up * jumpForce * 0.5f);
+    }
+
+
+    #region Helper Methods
+    // make lateral movement use 2D vectors??
+    private void Accelerate(Vector3 direction)
+    {       
+        if(grounded)
+        {
+            AddVelocity(direction.normalized * maxSpeed * acceleration);
+        }
+        else
+        {
+            AddVelocity(direction.normalized * maxSpeed * acceleration * 0.25f);
+        }
+
+        velocity2D = new Vector2(body.velocity.x, body.velocity.z);
+
+        if (velocity2D.sqrMagnitude > maxSpeed * maxSpeed)
+        {
+            velocity2D = velocity2D.normalized * maxSpeed;
+            body.velocity = new Vector3(velocity2D.x, body.velocity.y, velocity2D.y);
         }
     }
 
@@ -96,41 +239,37 @@ public class PlayerController : MonoBehaviour
             body.velocity = Vector3.zero;
         }
     }
-
-    private void Accelerate(Vector3 direction)
+    private void Decelerate(Vector3 direction)
     {
-        AddVelocity(direction.normalized * maxSpeed * acceleration);
-
-        if (body.velocity.sqrMagnitude < maxSpeed * maxSpeed)
+        if (body.velocity.sqrMagnitude > 0.1f)
         {
-            body.velocity = body.velocity.normalized * maxSpeed;
+            AddVelocity(direction.normalized * maxSpeed * -deceleration); // decelerate by a percentage of maxSpeed per tick
+        }
+        else
+        {
+            body.velocity = Vector3.zero;
         }
     }
 
     private void AddVelocity(Vector3 v)
     {
-        body.velocity += v;
+        body.velocity += v * Time.deltaTime;
     }
 
-
-    private void Jump()
+    private void LockMouse()
     {
-        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            grounded = false;
-            body.AddForce(transform.up * jumpForce);
-        }
-        else if(!grounded) // apply gravity
-        {
-            body.velocity += gravity;
+            if (Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
         }
     }
+    #endregion
 
-    private void WallBonk()
-    {
-        body.velocity = Vector3.zero;
-    }
 
+    #region Collision Resolution
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
@@ -141,8 +280,16 @@ public class PlayerController : MonoBehaviour
         // check for bonk
         if (!grounded && collision.gameObject.CompareTag("Wall"))
         {
-            WallBonk();
+            
+            WallBonk(collision.relativeVelocity);
         }
+
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            EnemyStomp();
+        }
+
+        body.drag = 1.0f;
     }
 
     private void OnCollisionExit(Collision collision)
@@ -151,5 +298,8 @@ public class PlayerController : MonoBehaviour
         {
             grounded = false;
         }
+
+        body.drag = 0.0f;
     }
+    #endregion
 }
